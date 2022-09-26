eec2dac1699137839cd213f1eae42f75c847db7e
HenningHaaland <100269225+HenningHaaland@users.noreply.github.com>
2022-09-26 10:12:16 +0200

    Add files via upload

---
+++ Fys375-Lab-3.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 26 09:42:06 2022
+
+@author: Henni
+"""
+
+echo "# Fys375-Lab-3" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/HenningHaaland/Fys375-Lab-3.git
+git push -u origin main
